917a1caa7c6f91976411d5a37ec3aeedf05c9666
iVyz3r <ilhqxmail@gmail.com>
2026-08-21 23:15:13 -0400

    Add Utils/WinRTTitle.hpp

---
+++ Utils/WinRTTitle.hpp
@@ -0,0 +1,10 @@
+/*
+Under an4rch Development Public Source License 1.0
+*/
+
+#pragma once
+#include <windows.h>
+
+namespace WinRTTitle {
+    void SetTitle(const wchar_t* title, HWND hwnd = nullptr);
+}
